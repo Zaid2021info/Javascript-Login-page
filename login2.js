0a08948b5632e87927ea62ea9e6c94671589c0c3
Muhammad Zaid Charoliya <muhammadzaid.charoliya@gmail.com>
2023-12-02 09:23:34 +0530

    first commit

---
+++ login2.js
@@ -0,0 +1,17 @@
+function validate()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if(username=="Zaid" & password=="1234")
+{
+    alert("login succesfully");
+    
+
+}
+else
+{
+    alert("login failed");
+}
+
+
+}
